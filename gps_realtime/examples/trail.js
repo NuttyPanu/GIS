288d01e5c34dcf48fd5156a6a081b391b5171c4e
NuttyPanu <misterpanukorn@gmail.com>
2019-02-04 11:15:30 +0700

    Add files via upload

--- gps_realtime/examples/trail.js
+++ gps_realtime/examples/trail.js
@@ -11,10 +11,12 @@ var map = L.map('map'),
     },
     realtime = L.realtime(function(success, error) {
         fetch('https://wanderdrone.appspot.com/')
+			//{"geometry": {"type": "Point", "coordinates": [-69.0553905407372, 28.80601349896117]}, "type": "Feature", "properties": {}}
         .then(function(response) { return response.json(); })
         .then(function(data) {
             var trailCoords = trail.geometry.coordinates;
             trailCoords.push(data.geometry.coordinates);
+			alert(trailCoords[0]+'-'+trailCoords[1]+'-'+trailCoords[2]);
             trailCoords.splice(0, Math.max(0, trailCoords.length - 5));
             success({
                 type: 'FeatureCollection',
